cc447756cbe7a102bd226e672427193c3eb33853
SaravjiUW <59896389+SaravjiUW@users.noreply.github.com>
2020-01-16 17:01:40 -0800

    Add files via upload

---
+++ sequence_01.py
@@ -0,0 +1,12 @@
+
+strData = str('0123')  # creates a string type
+print('string:', strData)
+
+rngData = range(4)  # creates a range type
+print('range: ', rngData)
+
+tplData = tuple('0123')  # creates a tuple type
+print('tuple: ', tplData)
+
+lstData = list('0123')  # creates a list type
+print('list: ', lstData)
